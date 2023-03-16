a481a20880b4200eb5e3834dd5f0d2d97edf2868
iamausafhussain <ausafhussain010@gmail.com>
2023-03-16 15:26:13 +0530

    added fixes

--- ExceptioniHandlingClass/Program.cs
+++ ExceptioniHandlingClass/Program.cs
@@ -1,22 +1,62 @@
-﻿namespace ExceptioniHandlingClass
+﻿using System.Text.RegularExpressions;
+
+namespace ExceptioniHandlingClass
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int number_one = 10, number_two = 0;
+            int flag = 0, second_flag = 0;
+            User user = new User();
 
-            try
-            {
-                int sum = number_one / number_two;
 
-                Console.WriteLine("Next line");
-            }
-            catch (Exception ex)
+            while (second_flag != 1)
             {
-                Console.WriteLine(ex.Message);
+                try
+                {
+                    if (flag == 0)
+                    {
+                        flag = 1;
+                        user.validateUser("ausafhussain010@gmail.com");
+                    }
+                    else
+                    {
+                        second_flag = 1;
+                        user.validatePassword("hello");
+                    }
+                }
+                catch (CustomExceptionHandler e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
-            finally { Console.WriteLine("Executed!!"); }
         }
+    }
+}
+
+public class CustomExceptionHandler : Exception
+{
+    public CustomExceptionHandler(String message) : base(message) { }
+}
+
+public class User
+{
+    public void validateUser(string username)
+    {
+        string pattern = "ausaf";
+        Regex regex = new Regex(pattern);
+
+        if (!regex.IsMatch(username))
+            throw new CustomExceptionHandler("Your email is invalid!");
+        else
+            Console.WriteLine("Valid username!");
+    }
+
+    public void validatePassword(string password)
+    {
+        if (password.Length < 8)
+            throw new CustomExceptionHandler("Your password length is less than 8 characters!");
+        else
+            Console.WriteLine("Valid password!");
     }
 }
