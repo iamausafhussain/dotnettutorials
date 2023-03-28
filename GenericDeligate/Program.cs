0e688d054bb7c07ec9d8096b313cf5d4924a4533
iamausafhussain <ausafhussain010@gmail.com>
2023-03-28 11:15:05 +0530

    added files

---
+++ GenericDeligate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericDeligate
+{
+    internal class Program
+    {
+        public delegate void CustomDel(int first_number, int second_number);
+        public static void Sum(int first_number, int second_number)
+        {
+            Console.WriteLine($"The sum is {first_number + second_number}");
+        }
+        public static void Product(int first_number, int second_number)
+        {
+            Console.WriteLine($"The product is {first_number * second_number}");
+        }
+        static void Main(string[] args)
+        {
+            CustomDel del = Program.Sum;
+            del += Program.Product;
+            del.Invoke(5, 3);
+        }
+    }
+}
