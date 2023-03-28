9074eadd63608499dac7ef73c9fa678a17f0ce7c
iamausafhussain <ausafhussain010@gmail.com>
2023-03-28 11:45:32 +0530

    added deligate files

---
+++ GenericFunC/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace GenericFunC
+{
+    internal class Program
+    {
+        //public delegate int CustomDel(int first_number, int second_number);
+        public static int Sum(int first_number, int second_number)
+        {
+            return first_number + second_number;
+        }
+        public static void Product(int first_number, int second_number)
+        {
+            Console.WriteLine($"The product is {first_number * second_number}");
+        }
+        static void Main(string[] args)
+        {
+            /*CustomDel obj = Sum;
+            int result = obj.Invoke(10, 5);
+            Console.WriteLine($"The Sum is {result}");*/
+
+            Func<int, int, int> obj = (x, y) =>
+            {
+                return x + y;
+            };
+
+            int result = obj.Invoke(10, 4);
+            Console.WriteLine("Result " + result);
+
+            Action<int, int> action_obj = (x, y) =>
+            {
+                Console.WriteLine(x + y);
+            };
+            action_obj.Invoke(10, 20);
+
+            Func<int, bool> predicate_obj = (x) =>
+            {
+                if (x > 5)
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            bool p_result = predicate_obj.Invoke(3);
+            Console.WriteLine(p_result);
+            /*Predicate<int> predicate_obj = (x) =>
+            {
+                if (x > 5)
+                {
+                    return true;
+                }
+                return false;
+            };
+            bool p_result = predicate_obj.Invoke(3);
+            Console.WriteLine(p_result);*/
+        }
+    }
+}
